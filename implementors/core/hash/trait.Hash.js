82885934109315eef9859dde474a2cb520e5070e
Travis CI User <travis@example.org>
2018-05-17 02:42:38 +0000

    Documentation for messense/unidiff-rs@f5d6a16

---
+++ implementors/core/hash/trait.Hash.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["aho_corasick"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"aho_corasick/struct.Match.html\" title=\"struct aho_corasick::Match\">Match</a>",synthetic:false,types:["aho_corasick::autiter::Match"]},];
+implementors["unidiff"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"unidiff/struct.Line.html\" title=\"struct unidiff::Line\">Line</a>",synthetic:false,types:["unidiff::Line"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"unidiff/struct.Hunk.html\" title=\"struct unidiff::Hunk\">Hunk</a>",synthetic:false,types:["unidiff::Hunk"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"unidiff/struct.PatchedFile.html\" title=\"struct unidiff::PatchedFile\">PatchedFile</a>",synthetic:false,types:["unidiff::PatchedFile"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
